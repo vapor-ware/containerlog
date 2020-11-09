f3f3ef815401674af087f7c8cd734bee66614c47
Erick Daniszewski <edaniszewski@gmail.com>
2020-11-09 07:47:28 -0500

    bump: 0.3.0

--- containerlog/__init__.py
+++ containerlog/__init__.py
@@ -15,7 +15,7 @@ from typing import Dict, Optional
 
 # Project attributes
 __title__ = 'containerlog'
-__version__ = '0.2.3'
+__version__ = '0.3.0'
 __description__ = 'Optimized, opinionated structured logging for containerized applications.'
 __author__ = 'Erick Daniszewski'
 __author_email__ = 'erick@vapor.io'
